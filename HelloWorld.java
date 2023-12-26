8ee075ad390be6aa2b525abbd86da6fdc7392210
slsabel <slsabelhesham50@gmail.com>
2023-12-26 14:12:37 +0200

    add print hello git at hello world class

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,9 @@ class HelloSwift{
 	public void printHelloWorld(){
 			System.out.println("Hello World!");
 	}
+	public void printHelloGit(){
+			System.out.println("Hello Git!");
+	}
 	public static void main(String[] args)
 	{
 		printHelloWorld();
