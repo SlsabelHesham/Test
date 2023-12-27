f0a66aa4b57713f08a7e26ebab7212b3edc27442
Mahmoud Hussein <mhussi201@gmail.com>
2023-12-27 14:06:22 +0200

    'version'

---
+++ mahmoud.java
@@ -0,0 +1,25 @@
+
+class HelloWorld{
+	
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+		
+	}
+}
+class HelloITI {
+
+	public void printITI(){
+		System.out.println("Hello ITI");
+		///////////////comment
+	}
+	public void printHelloGit(){
+		System.out.println("Hello GIT");
+		System.out.println("Hello GIT");
+		
+	}
+	public void printHelloMahmoud(){
+		System.out.println("Hello GIT");
+		System.out.println("Hello GIT");
+		
+	}
+}
